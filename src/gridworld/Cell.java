9d29755978d82d2962334a260e86a0273d0adcda
Eric Zimmerman <32339982+egz9@users.noreply.github.com>
2019-02-18 14:11:04 -0500

    generate gridworld

---
+++ src/gridworld/Cell.java
@@ -0,0 +1,32 @@
+package gridworld;
+
+public class Cell {
+	
+	//x coordinate in grid
+	int x;
+	 
+	//y coordinate in grid
+	int y;
+	
+	//heuristic. The manahattan distance to the goal cell. calculated with:
+	//|this.x - goal.x| + |this.y - goal.y|
+	int h;
+	
+	//initially -1 to indicate infinity. Length of the shortest path from
+	//starting cell to this cell
+	int g;
+	
+	//an estimate of the distance from the start cell to the goal cell via
+	//this cell. f = g + h
+	int f;
+	
+	//true if cell is blocked. It is unreachable by the agent
+	boolean isBlocked;
+	
+	public Cell (int x, int y, int h){
+		this.x = x;
+		this.y = y;
+		this.h = h;
+	}
+}
+
---
+++ src/gridworld/Grid.java
@@ -0,0 +1,166 @@
+package gridworld;
+import java.util.Random;
+import java.util.Stack;
+
+
+public class Grid {
+	Cell[][] grid;
+	
+	public Grid(int rows, int columns){
+		grid = new Cell[rows][columns];
+		for (int i = 0; i < rows; i++){
+			for (int j = 0; j < columns; j++){
+				grid[i][j] = new Cell(i,j,-1);//h = -1 temporarily
+			}
+		}
+	}
+	
+	public void printGrid(){
+		if (grid == null)
+			return;
+		for (int i = 0; i < grid.length; i++){
+			for (int j = 0; j < grid[i].length; j++){
+				if ( !grid[i][j].isBlocked ){
+					System.out.print("_ ");
+				} 
+				else {
+					System.out.print("X ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public void generateMaze(){
+		boolean[][] visited = new boolean[grid.length][grid[0].length];
+		Random rand = new Random();
+		
+		//start at random node
+		int x = rand.nextInt(grid.length);
+		int y = rand.nextInt(grid[0].length);
+		dfs(x, y, visited);
+		
+		
+	}
+	
+	private boolean cellHasUnvisitedNeighbors(Cell current, boolean[][] visited){
+		try {
+			if (!visited[current.x][current.y+1])
+				return true;
+		}
+		catch (IndexOutOfBoundsException e){}
+		try {
+			if (!visited[current.x+1][current.y])
+				return true;
+		}
+		catch (IndexOutOfBoundsException e){}
+		try {
+			if (!visited[current.x][current.y-1])
+				return true;
+		}
+		catch (IndexOutOfBoundsException e){}
+		try {
+			if (!visited[current.x-1][current.y])
+				return true;
+		}
+		catch (IndexOutOfBoundsException e){}
+		return false;
+	}
+	
+	
+	private Cell getUnvisitedCell(boolean[][] visited){
+		for(int i = 0; i < visited.length; i++){
+			for(int j = 0; j < visited[0].length; j++){
+				if (!visited[i][j]){
+					return grid[i][j];
+				}
+			}
+		}
+		return null;
+	}
+
+	
+	public void dfs(int x, int y, boolean[][] visited){
+		
+		
+		//visited[x][y] = true;
+		Stack<Cell> stack = new Stack<Cell>();
+		Random rand = new Random();
+		stack.push(grid[x][y]);
+		Cell current;
+		while (getUnvisitedCell(visited) != null){
+			if (stack.isEmpty()){
+				stack.push(getUnvisitedCell(visited));
+			}
+			current = stack.pop();
+			if ( visited[current.x][current.y] ){ 
+				continue;
+			}
+				
+			visited[current.x][current.y] = true;
+			if (!cellHasUnvisitedNeighbors(current, visited)){
+				continue;
+			}
+			
+			//check right
+			
+			//choose random neighbor. 0=up, 1=right, 2=down, 3=left
+			int n = rand.nextInt(4);
+			
+			
+			
+			//cell above
+			if (n == 0 && current.y + 1 < grid[0].length && !visited[current.x][current.y+1]){
+				//mark cell as blocked with 30% probability
+				if (rand.nextInt(10) + 1 <= 5){
+					grid[current.x][current.y+1].isBlocked = true;
+					visited[current.x][current.y+1] = true;
+				}
+				else {
+					stack.push(grid[current.x][current.y+1]);
+				}
+			}
+			
+			//cell to the right
+			if (n == 1 && current.x + 1 < grid.length && !visited[current.x+1][current.y]){
+				if (rand.nextInt(10) + 1 <= 5){
+					grid   [current.x+1][current.y].isBlocked = true;
+					visited[current.x+1][current.y] = true;
+				}
+				else {
+					stack.push(grid[current.x+1][current.y]);
+				}
+			}
+			
+			//cell underneath
+			if (n == 2 && current.y - 1 >= 0 && !visited[current.x][current.y-1]){
+				if (rand.nextInt(10) + 1 <= 5){
+					grid   [current.x][current.y-1].isBlocked = true;
+					visited[current.x][current.y-1] = true;
+				}
+				else {
+					stack.push(grid[current.x][current.y-1]);
+				}
+			}
+			
+			//cell left
+			if (n == 3 && current.x - 1 >= 0 && !visited[current.x-1][current.y]){
+				if (rand.nextInt(10) + 1 <= 5){
+					grid   [current.x-1][current.y].isBlocked = true;
+					visited[current.x-1][current.y] = true;
+				}
+				else {
+					stack.push(grid[current.x-1][current.y]);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		Grid myGrid = new Grid(101, 101);
+		//myGrid.printGrid();
+		myGrid.generateMaze();
+		myGrid.printGrid();
+	}
+}
