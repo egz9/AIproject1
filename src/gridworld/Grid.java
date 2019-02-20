04c08db989014b26bd215335daf62fbfa2b8c748
Eric Zimmerman <32339982+egz9@users.noreply.github.com>
2019-02-19 22:40:48 -0500

    added agent and target fields

--- src/gridworld/Grid.java
+++ src/gridworld/Grid.java
@@ -12,9 +12,13 @@ import java.util.Stack;
 
 
 public class Grid implements Serializable {
+	private static final long serialVersionUID = 2L;
 	Cell[][] grid;
+	Cell agent;
+	Cell target;
 	
 	public Grid(int rows, int columns){
+		agent = target = null;
 		grid = new Cell[rows][columns];
 		for (int i = 0; i < rows; i++){
 			for (int j = 0; j < columns; j++){
@@ -42,18 +46,22 @@ public class Grid implements Serializable {
         return g;
 	}
 	
+	//prints grid with X's as blocked cells and _'s as open cells
+	//A represents the agent and T represents the target
+	//grid is printed so that [0][0] IS UPPER LEFT CORNER and x position
+	//is first index and y position is second index i.e. [x][y]
 	public void printGrid(){
 		if (grid == null)
 			return;
-		for (int i = 0; i < grid.length; i++){
-			for (int j = 0; j < grid[i].length; j++){
-				if ( !grid[i][j].isBlocked ){
-					//*************************************
-					//if (grid[i][j].f == -1){
-						//System.out.print("O ");
-						//continue;
-					//}
-					//**************************************
+		for (int j = 0; j < grid[0].length; j++){
+			for (int i = 0; i < grid.length; i++){
+				if (grid[i][j] == agent){
+					System.out.print("A ");
+				}
+				else if (grid[i][j] == target){
+					System.out.print("T ");
+				}
+				else if ( !grid[i][j].isBlocked ){
 					System.out.print("_ ");
 				} 
 				else {
@@ -65,6 +73,7 @@ public class Grid implements Serializable {
 		System.out.println();
 	}
 	
+	//generate's maze using a dfs approach
 	public void generateMaze(){
 		boolean[][] visited = new boolean[grid.length][grid[0].length];
 		Random rand = new Random();
@@ -77,6 +86,7 @@ public class Grid implements Serializable {
 		
 	}
 	
+	//helper for dfs method. returns true if cell has a neighbor that has not been visited yet
 	private boolean cellHasUnvisitedNeighbors(Cell current, boolean[][] visited){
 		try {
 			if (!visited[current.x][current.y+1])
@@ -101,7 +111,7 @@ public class Grid implements Serializable {
 		return false;
 	}
 	
-	
+	//helper for dfs method. returns an unvisited cell or null if none exist
 	private Cell getUnvisitedCell(boolean[][] visited){
 		for(int i = 0; i < visited.length; i++){
 			for(int j = 0; j < visited[0].length; j++){
@@ -113,7 +123,8 @@ public class Grid implements Serializable {
 		return null;
 	}
 
-	
+	//starts from cell at x,y and begins visiting neighbors with a 30% chance of
+	//turning an unblocked cell into a blocked cell
 	public void dfs(int x, int y, boolean[][] visited){
 		
 		
@@ -122,7 +133,12 @@ public class Grid implements Serializable {
 		Random rand = new Random();
 		stack.push(grid[x][y]);
 		Cell current;
+		
+		//on each iteration of this loop a cell is either poped of the stack or retrieved
+		//with the getUnvisitedCell method. This cell is marked as visited and some
+		//of its neighbors may be randomly chosen to be blocked.
 		while (getUnvisitedCell(visited) != null){
+			
 			if (stack.isEmpty()){
 				stack.push(getUnvisitedCell(visited));
 			}
@@ -130,17 +146,15 @@ public class Grid implements Serializable {
 			if ( visited[current.x][current.y] ){ 
 				continue;
 			}
-				
 			visited[current.x][current.y] = true;
-			//current.f = -1;//*****************************************
 			if (!cellHasUnvisitedNeighbors(current, visited)){
 				continue;
 			}
 			
-			//check right
-			
-			
-			
+			//visit each neighbor in random order. If a randomly chosen neighbor is
+			//chosen to be blocked its marked as blocked and visited. If the neighbor
+			//is chosen to be unblcoked it will be pushed on the stack and we will break
+			//out of the loop. This is the next cell that our method will look it
 			int iterations = 0;
 			while (iterations < 4) {
 				//choose random neighbor. 0=up, 1=right, 2=down, 3=left
@@ -200,24 +214,41 @@ public class Grid implements Serializable {
 	}
 	
 	public static void main(String[] args){
+		
+		
+		Grid myGrid;
+		myGrid = new Grid(3, 5);
+		myGrid.printGrid();
+		try {
+			myGrid = loadFromFile("grids" + File.separator + "test1");
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		myGrid.printGrid();
+		myGrid.agent = myGrid.grid[0][3];
+		myGrid.target = myGrid.grid[19][18];
+		myGrid.printGrid();
+		
+		
+		
+		
+		/********************************************************************
+		//write mazes to auto-generated files
 		Grid myGrid = new Grid(20, 20);
-		//myGrid.printGrid();
-		//myGrid.generateMaze();
-		//myGrid.printGrid();
 		for (int i = 0; i < 5; i++){
 			try {
 				myGrid = new Grid(20, 20);
 				myGrid.generateMaze();
 				myGrid.printGrid();
 				myGrid.writeToFile("grids" + File.separator + "test" + (i+1));
-				//myGrid = loadFromFile("grids" + File.separator + "grid1");
-				//myGrid.printGrid();
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+		*/
 		
 	}
 }
